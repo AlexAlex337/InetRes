48ecac47f8e3c9e389ef5919e80ea69a18557ffa
AlexAlex337 <75129560+AlexAlex337@users.noreply.github.com>
2021-04-05 15:24:04 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Автомойка') }}</title>
-
+    
+    <style>
+        .invalid-feedback {
+        color: red;
+        }
+    </style>
+    
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
