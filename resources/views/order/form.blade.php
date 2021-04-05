82169318cfbc0683e0ed4025ed4ba5fcc3220e24
AlexAlex337 <75129560+AlexAlex337@users.noreply.github.com>
2021-04-05 16:38:50 +0300

    Update form.blade.php

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -26,7 +26,7 @@
 
             <div class="row">
                 <label>Дата и время:</label>
-                <input name="time" type="datetime-local" min=<?php echo date('Y-m-d\TH:i:sP', $row['Time']);?>>
+                <input name="time" type="datetime-local" min=<?php echo date('Y-m-d\TH:i');?>>
             </div>
             @error('time')
                 <span class="red-text">{{ $errors->first('time') }}</span><br><br>
