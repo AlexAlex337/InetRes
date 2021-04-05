3eed6292cf0865880847e1aa58c1f7a9c74c5b7d
AlexAlex337 <75129560+AlexAlex337@users.noreply.github.com>
2021-04-05 16:08:57 +0300

    Update form.blade.php

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -26,7 +26,7 @@
 
             <div class="row">
                 <label>Дата и время:</label>
-                <input name="time" type="datetime-local" min="<?php echo $currenttime?>>
+                <input name="time" type="datetime-local">
             </div>
             @error('time')
                 <span class="red-text">{{ $errors->first('time') }}</span><br><br>
