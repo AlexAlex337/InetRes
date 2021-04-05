c18b47ab475f21378163d3b0cde530c8b2e07045
AlexAlex337 <75129560+AlexAlex337@users.noreply.github.com>
2021-04-05 15:42:36 +0300

    Update contacts.blade.php

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -33,10 +33,10 @@
                     </tr>
                 </table>
 				
-            <div class="col s6 right">
+            <div>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d734.8801966273545!2d49.66749524854306!3d58.603006351698056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa68e3999f250f620!2z0JLRj9GC0YHQutC40Lkg0JPQvtGB0YPQtNCw0YDRgdGC0LLQtdC90L3Ri9C5INCj0L3QuNCy0LXRgNGB0LjRgtC10YIgKNC60L7RgNC_0YPRgSAxKQ!5e0!3m2!1sru!2sru!4v1615143298322!5m2!1sru!2sru"
-                    width=100% height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    max-width=1200px width=100% height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
     </div>
