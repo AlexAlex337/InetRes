7fd0f5c099a395000b4d01026807064b5a9f0d6c
AlexAlex337 <75129560+AlexAlex337@users.noreply.github.com>
2021-04-05 15:16:45 +0300

    Update reset.blade.php

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -18,11 +18,6 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-                            <style>
-                                .invalid-feedback {
-                                color: red;
-                                }
-                            </style>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
