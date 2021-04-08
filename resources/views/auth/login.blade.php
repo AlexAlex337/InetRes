8b04de431d331d84721b90c2a0cac20fc4fbe141
AlexAlex337 <75129560+AlexAlex337@users.noreply.github.com>
2021-04-08 17:25:51 +0300

    Update login.blade.php
    change login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
             <div class="row">
-                <input placeholder="E-mail" id="email" type="email"
+                <input placeholder="E-mail" id="email" type="email" pattern="[а-яa-z0-9._%+-]+@[а-яa-z0-9.-]+\.[а-яa-z]{2,4}$"
                     class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"
                     required autocomplete="email" autofocus>
                 @error('email')
